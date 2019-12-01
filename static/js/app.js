fc74b7ba4c3df556806712284a9bf117a2db3e11
Jean Ventura <jv@venturasystems.net>
2019-11-30 19:20:41 -0800

    Add template literals

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 // Reload page with search input as q param
 function queryRedirect() {
     // Get query from input field
-    const query = "q=" + document.getElementById("unitsQuery").value;
+    const query = `q=${document.getElementById("unitsQuery").value}`;
     // Split URL
     const splitUrl = window.location.href.split("?");
     // Base URL (without params)
@@ -14,6 +14,6 @@ function queryRedirect() {
     // Join query param to the other params
     const params = [baseParams, query].join('&');
     // Join everything and redirect
-    location = baseUrl + "?" + (baseParams ? params : query);
+    location = `${baseUrl}?${baseParams ? params : query}`;
     return false;  // Avoid default behavior
 }
