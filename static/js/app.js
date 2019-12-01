1b56e0dc2238efd4d45a87a40f6007c5b0e7d748
Jean Ventura <jv@venturasystems.net>
2019-11-30 18:39:10 -0800

    Add on submit behavior for search

---
+++ static/js/app.js
@@ -0,0 +1,19 @@
+// Reload page with search input as q param
+function queryRedirect() {
+    // Get query from input field
+    const query = "q=" + document.getElementById("unitsQuery").value;
+    // Split URL
+    const splitUrl = window.location.href.split("?");
+    // Base URL (without params)
+    const baseUrl = splitUrl[0];
+    // Params for URL (excluding q param)
+    const baseParams = (splitUrl.length > 1 ? splitUrl[1] : "")
+      .split("&")
+      .filter(param => !param.startsWith("q="))
+      .join("&");
+    // Join query param to the other params
+    const params = [baseParams, query].join('&');
+    // Join everything and redirect
+    location = baseUrl + "?" + (baseParams ? params : query);
+    return false;  // Avoid default behavior
+}
