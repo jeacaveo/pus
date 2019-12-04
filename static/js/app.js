864c2e2c77b20374ca5069c200745d67759ba9a3
Jean Ventura <jv@venturasystems.net>
2019-12-03 18:36:40 -0800

    Add fetching and showing of units

--- static/js/app.js
+++ static/js/app.js
@@ -1,19 +1,55 @@
+// Create unit element
+function addUnitToPage(unit) {
+  const innerDiv = document.createElement("div");
+  innerDiv.classList.add("card");
+  innerDiv.classList.add("col-md-4");
+  imageHtml = `
+    <img src="${unit.panel_url}" class="card-img-top" alt="${unit.name}">`
+  nameHtml = `<h5 class="card-title">${unit.name}</h5>`
+  innerDiv.innerHTML = `
+    ${unit.panel_url ? imageHtml : nameHtml}
+    <p class="card-text text-warning">Supply: ${unit.supply}</p>`;
+  document.getElementById("unitDisplay").appendChild(innerDiv);
+  return innerDiv;
+}
+
+
+// Handle post-load actions
+function onLoad() {
+  const url = new URL(window.location.href);
+  const params = new URLSearchParams(url.search.slice(1));
+  if (params.has("q")) {
+    // Hide cheatsheet
+    document.getElementById("cheatsheet").classList.add("d-none");
+
+    // Show loading spinner
+    const spinner = document.getElementById("unitsSpinner");
+    spinner.classList.add("d-flex");
+
+    // Call API
+    const url = new URL('https://papi.ancobl.in/api/latest/units/');
+    fetch(
+        `${url.toString()}?q=${params.get("q")}`,
+        {headers: {'Content-Type': 'application/json'}})
+      .then(response => response.json())
+        .then(data => {
+          spinner.classList.remove("d-flex");  // Hide loading spinner
+          data.map(addUnitToPage);
+        }).catch(err => console.log(err));
+  }
+}
+
+
 // Reload page with search input as q param
 function queryRedirect() {
-    // Get query from input field
-    const query = `q=${document.getElementById("unitsQuery").value}`;
-    // Split URL
-    const splitUrl = window.location.href.split("?");
-    // Base URL (without params)
-    const baseUrl = splitUrl[0];
-    // Params for URL (excluding q param)
-    const baseParams = (splitUrl.length > 1 ? splitUrl[1] : "")
-      .split("&")
-      .filter(param => !param.startsWith("q="))
-      .join("&");
-    // Join query param to the other params
-    const params = [baseParams, query].join('&');
-    // Join everything and redirect
-    location = `${baseUrl}?${baseParams ? params : query}`;
+    const url = new URL(window.location.href);
+    const params = new URLSearchParams(url.search.slice(1));
+
+    // Remove existing q param and replace with the current search value
+    params.delete("q");
+    params.append("q", document.getElementById("unitsQuery").value);
+
+    // Re-join everything and redirect
+    location = `${url.pathname}?${params.toString()}`;
     return false;  // Avoid default behavior
 }
